57a41bfb1984f98f020c742f9a719c296e907615
wingFire29 <paragshrivastava727@gmail.com>
2024-10-13 10:58:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,85 @@
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GymSathi</title>
+    <style>
+        .header{
+            display: flex;
+            flex direction: column;
+            align-items:center:
+        }
+    </style>
+</head>
+<body>
+<div class ="header">
+    <h1>
+        header
+    </h1>
+    <img src="C:\Users\ABC\OneDrive\Desktop\hackthon ggi\download.jpg" alt="description">
+</div>
+       <p> <details>
+            <summary>HOMEPAGE</summary>
+        </details>
+         <details>
+            <summary>Fitness</summary>
+        </details>
+
+        <details>
+            <summary>Tracking</summary>
+        </details>
+    </p>
+    
+
+</body>
+</html> -->
+
+
+<!-- 
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gym Sathi</title>
+   <style>
+     *{
+    margin: 0;
+    padding: 0;
+    font-family: 'poppins',sans-serif;
+    box-sizing: border-box;
+ }
+ .container{
+ width: 100%;
+min-height: 100vh;
+/* url m image ka path bhejo */
+background-image:linear-gradient(rgba(9,0,77,0,65),rgba(9,0,77,0,65)),url(hack/background.png);
+background-size: cover;
+background-position: center;
+padding:  10 px 8%;
+
+ }
+   </style>
+</head>
+<body>
+    <div class="container">
+         <nav>
+           
+            <img src=""C:\Users\ABC\OneDrive\Desktop\hackthon ggi\logo.png.jpg"" class="logo"> 
+            <ul>
+                <li><a  href="#">Home</a></li>
+                <li><a  href="#">Fitness</a></li>
+                <li><a  href="#">Diet</a></li>
+                <li><a  href="#">Tracking</a></li>
+            </ul>
+           
+             <button class="btn"><img src=""C:\Users\ABC\OneDrive\Desktop\hackthon ggi\360_F_102528344_XfIuLtPSSBIIR3NYtLRe1QOBUgkdS8Cp.jpg"">profile</button> 
+         </nav> 
+
+    </div>
+
+
+</body>
+</html> -->
